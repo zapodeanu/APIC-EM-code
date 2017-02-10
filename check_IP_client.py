940f8c922539633e3c7b0091484d6fc1c6aa5aff
gzapodea <gzapodea@cisco.com>
2017-02-10 11:14:35 -0800

    this updated version is ready for testing

---
+++ check_IP_client.py
@@ -0,0 +1,98 @@
+# developed by Gabi Zapodeanu, Cisco Systems, TSA, GPO
+
+# !/usr/bin/env python3
+
+import requests
+import json
+import requests.packages.urllib3
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable insecure https warnings
+
+# Declarations for the controller info. The values will need to change for the proper Sandbox info,
+# or the customer controller
+
+APIC_EM = 'sandboxapic.cisco.com/api/v1'
+APIC_EM_USER = 'devnetuser'
+APIC_EM_PASSW = 'Cisco123!'
+
+# client IP addresses to test 10.2.1.22 - ethernet
+# client IP addresses to test 10.1.15.117 - wifi
+
+
+def get_service_ticket():
+    """
+    This function will generate the Auth ticket required to access APIC-EM
+    API call to /ticket is used to create a new user ticket
+    :return: ticket
+    """
+
+    payload = {'username': APIC_EM_USER, 'password': APIC_EM_PASSW}
+    url = 'https://' + APIC_EM + '/ticket'
+    header = {'content-type': 'application/json'}
+    ticket_response = requests.post(url, data=json.dumps(payload), headers=header, verify=False)
+    if not ticket_response:
+        print('No data returned!')
+    else:
+        ticket_json = ticket_response.json()
+        ticket = ticket_json['response']['serviceTicket']
+        print('Created APIC-EM ticket: ', ticket)
+        return ticket
+
+
+def get_input_ip():
+    """
+    This function will ask the user to input the IP address. The format of the IP address is not validated
+    The function will return the IP address
+    :return: the IP address
+    """
+
+    ip_address = input('Input the IP address to be validated?  ')
+    return ip_address
+
+
+def check_client_ip_address(client_ip):
+    """
+    The function will find out if APIC-EM has a client device configured with the specified IP address.
+    API call to /host
+    It will print if a client device exists or not.
+    :param client_ip: client IP address
+    :return: None
+    """
+
+    url = 'https://' + APIC_EM + '/host'
+    header = {'accept': 'application/json', 'X-Auth-Token': APIC_EM_TICKET}
+    payload = {'hostIp': client_ip}
+    host_response = requests.get(url, params=payload, headers=header, verify=False)
+    host_json = host_response.json()
+    if not host_json['response']:
+        print('The IP address ', client_ip, ' is not used by any client devices')
+    else:
+        print('The IP address ', client_ip, ' is used by a client device')
+        # print (json.dumps(host_json, indent=4, separators=(' , ', ' : ')))    # sample print json output, optional
+
+
+def main():
+    """
+    This simple script will find out if there is a client connected to the Enterprise network
+    It will ask the user to input an IP address for a client device.
+    It will print information if the input iP address is being used by a client or not
+    """
+
+    # create an auth ticket for APIC-EM
+
+    global APIC_EM_TICKET    # make the ticket a global variable in this module
+    APIC_EM_TICKET = get_service_ticket()
+
+    # input IP address for client
+
+    client_ip_address = get_input_ip()
+    print('IP Address to be validated:', client_ip_address)
+
+    # check if the input IP address is used by network clients
+
+    check_client_ip_address(client_ip_address)
+
+
+if __name__ == '__main__':
+    main()
