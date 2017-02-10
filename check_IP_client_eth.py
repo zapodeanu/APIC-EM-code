7cb4f7f816fa6c3925e9a03d6d2e31935079c65d
gzapodea <gzapodea@cisco.com>
2017-02-10 09:48:57 -0800

    renamed file, added few more lines of code

--- check_IP_client_eth.py
+++ check_IP_client_eth.py
@@ -11,3 +11,13 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests.auth import HTTPBasicAuth  # for Basic Auth
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable insecure https warnings
+
+def main():
+    """
+    This simple script will find out if there is a wired client ethernet connected to the Enterprise network
+    It will print information ...
+    """
+
+
+if __name__ == '__main__':
+    main()
